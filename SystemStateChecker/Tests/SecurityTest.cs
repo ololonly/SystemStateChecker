c66d7437eec43eac0c69383f8ace03b10629213c
ololonly <onlyno21@gmail.com>
2017-10-15 16:26:18 +0300

    additional fixes

--- SystemStateChecker/Tests/SecurityTest.cs
+++ SystemStateChecker/Tests/SecurityTest.cs
@@ -26,20 +26,7 @@ namespace SystemStateChecker.Tests
 
         public Task<string> ResultAsync()
         {
-            return Task.Run(() =>
-            {
-                using (FileStream fs = File.Create("test.txt"))
-                {
-                    using (var sw = new StreamWriter(fs))
-                    {
-                        sw.Write(@"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*");
-                    }
-                }
-                Thread.Sleep(1000);
-                return !File.Exists("test.txt")
-                    ? "Ваш антивирус успешно справился с угрозой"
-                    : "Ваш антивирус не работает";
-            });
+            return Task.Run(() => Result());
         }
 
     }
